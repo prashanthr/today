39811c3913f1f42f7217d4873340682571bfaf37
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-09-10 16:12:07 -0700

    wip working

--- src/api.rs
+++ src/api.rs
@@ -1,10 +1,15 @@
 
-use actix_web::{HttpResponse, Responder, http};
+use actix_web::{web, HttpResponse, Responder, http};
 use http::StatusCode;
 use std::collections::HashMap;
 use std::time::Duration;
+use std::env;
 extern crate reqwest;
+extern crate serde;
 use reqwest::{header, ClientBuilder};
+use serde::{Deserialize};
+// #[macro_use] 
+// extern crate serde_derive;
 
 async fn make_request (url: &str) -> Result<HashMap<String, String>, reqwest::Error> {
   let mut headers = header::HeaderMap::new();
@@ -40,14 +45,32 @@ async fn make_request (url: &str) -> Result<HashMap<String, String>, reqwest::Er
   }
 }
 
+// fn get_env (key: &str, defaultValue: &str) -> &str {
+//   let empty: &str = "";
+//   match env::var(key) {
+//     Ok(val) => {
+//       println!("{}: {}", key, val);
+//       &val
+//     },
+//     Err(e) => {
+//       println!("couldn't interpret {:?}: {}", key, e);
+//       if defaultValue.is_empty() {
+//         &empty
+//       } else {
+//         &defaultValue.to_owned()
+//       }
+//     },
+//   }
+// }
+
 pub async fn health() -> impl Responder {
   let status_data: HashMap<&str, &str> = [("status", "healthy")].iter().cloned().collect();
   HttpResponse::Ok().json(status_data)
 }
 
 pub async fn quote_of_day() -> impl Responder {
-  const QOD_URL: &str = "https://quotes.rest/qod?language=en";
-  match make_request(QOD_URL).await {
+  let qod_url: &str = "https://httpbin.org/ip"; //"https://quotes.rest/qod?language=en";
+  match make_request(qod_url).await {
     Ok(data) => {
       println!("Inner {:?}", data);
       HttpResponse::Ok()
@@ -57,9 +80,24 @@ pub async fn quote_of_day() -> impl Responder {
   }
 }
 
-pub async fn weather_of_day() -> impl Responder {
-  const WOD_URL: &str = "https://httpbin.org/ip";
-  match make_request(WOD_URL).await {
+#[derive(Deserialize)]
+pub struct WODRequest {
+  location: Option<String>
+}
+
+pub async fn weather_of_day(info: web::Query<WODRequest>) -> impl Responder {
+  println!("here {:?}", info.location);
+  let resolved_location: String = match &info.location {
+    None => String::from(""),
+    Some(loc) => loc.to_string(),
+  };
+  let base_url: &str = "https://api.openweathermap.org/data/2.5/weather";
+  //let api_key: &str = get_env(String::from("WEATHER_API_KEY"), String::from(""));
+  let api_key: &str = "db5a05bd35c42b20c0934a36d0735441";
+  let wod_url: &str = &(base_url.to_owned() + "?q=" + &resolved_location.to_owned() + "&appId=" + &api_key.to_owned());
+  println!("WOD URL: {}", wod_url);
+  //const WOD_URL: &str = format!("{}?q={}&appid={}", base_url, LOCATION, API_KEY);
+  match make_request(wod_url).await {
     Ok(data) => HttpResponse::Ok().json(data),
     Err(_err) => HttpResponse::new(StatusCode::from_u16(500).unwrap())
   }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::{web, App, HttpServer};
-extern crate reqwest;
+
+extern crate serde;
+// use serde::{Deserialize};
 
 mod api;
 
@@ -7,13 +9,19 @@ mod api;
 async fn main() -> std::io::Result<()> {
     const HOST: &str = "127.0.0.1";
     const PORT: i32 = 8088;
-
+    
+    // #[derive(Deserialize)]
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(api::health))
             .route("/health", web::get().to(api::health))
             .route("/qod", web::get().to(api::quote_of_day))
             .route("/wod", web::get().to(api::weather_of_day))
+            // .service(
+            //   web::resource("/wod")
+            //   .route(web::get())
+            //   .to(api::weather_of_day) // <- use `Query` extractor
+            // )
     })
     .bind(format!("{}:{}", HOST, PORT))?
     .run()
