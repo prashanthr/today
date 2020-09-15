57556c9b6bff83842c9532808f6c2d9c6fd52029
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-09-15 09:49:11 -0700

    feat(unified-api): [ch137] Build unified api (#6)
    Co-authored-by: Prashanth R <1409501+prashanthr@users.noreply.github.com>

--- src/api.rs
+++ src/api.rs
@@ -8,7 +8,14 @@ extern crate serde;
 use serde::{Deserialize, Serialize};
 
 use crate::util;
-use crate::types::{AppCache, QOD, WODRequest, WOD, NODRequest, NOD, HOD};
+use crate::types::{
+  AppCache, 
+  QOD, Quote,
+  WODRequest, WOD, 
+  NODRequest, NOD, 
+  HOD,
+  TodayRequest, TodayResponse
+};
 
 /* Route Handlers */
 
@@ -21,9 +28,9 @@ pub async fn health() -> impl Responder {
 }
 
 /*
-  Test Route
+  Debug/Test Route
 */
-pub async fn test(data: web::Data<Mutex<AppCache>>) -> impl Responder {
+pub async fn debug(data: web::Data<Mutex<AppCache>>) -> impl Responder {
   let app_cache = data.lock().unwrap();
   app_cache.print();
   #[derive(Serialize, Deserialize, Debug)]
@@ -40,31 +47,37 @@ pub async fn test(data: web::Data<Mutex<AppCache>>) -> impl Responder {
 /*
   Quote of day
 */
-pub async fn quote_of_day(data: web::Data<Mutex<AppCache>>) -> impl Responder {
+pub async fn get_qod(data: web::Data<Mutex<AppCache>>) -> Option<Vec<Quote>> {
   let mut app_cache = data.lock().unwrap();
   let qod_url: &str = "http://quotes.rest/qod.json?category=inspire&language=en";
   if app_cache.qod_exists() {
-    HttpResponse::Ok()
-          .json(app_cache.qod.as_ref())
+    app_cache.qod.clone()
   } else {
     match util::http_client::make_request::<QOD>(qod_url).await {
       Ok(data) => {
         app_cache.qod = Some(data.clone().contents.quotes);
         app_cache.qod_dt = Some(util::datetime::now());
-        HttpResponse::Ok()
-          .json(data.contents.quotes)
+        Some(data.contents.quotes)
       },
-      Err(_err) => HttpResponse::new(StatusCode::from_u16(500).unwrap())
+      Err(_err) => None
     }
+  }
+}
+
+pub async fn quote_of_day(data: web::Data<Mutex<AppCache>>) -> impl Responder {
+  match get_qod(data).await {
+    Some(result) => HttpResponse::Ok().json(result),
+    None => HttpResponse::new(StatusCode::from_u16(500).unwrap())
   }
 }
 
 /*
   Wrather of day
 */
-pub async fn weather_of_day(data: web::Data<Mutex<AppCache>>, info: web::Query<WODRequest>) -> impl Responder {
+
+pub async fn get_wod(data: web::Data<Mutex<AppCache>>, params: WODRequest) -> Option<WOD> {
   let mut app_cache = data.lock().unwrap();
-  let resolved_location: String = match &info.location {
+  let resolved_location: String = match &params.location {
     None => String::from("san francisco,usa"),
     Some(loc) => loc.to_string(),
   };
@@ -74,8 +87,11 @@ pub async fn weather_of_day(data: web::Data<Mutex<AppCache>>, info: web::Query<W
   let wod_url: &str = &(base_url.to_owned() + "?q=" + &resolved_location.to_owned() + "&APPID=" + &api_key.to_owned());
   
   if app_cache.wod_exists(resolved_location) {
-    HttpResponse::Ok()
-          .json(app_cache.wod.as_ref().unwrap().get(&resolved_location_cache))
+    Some(
+      app_cache
+        .wod.as_ref().unwrap().get(&resolved_location_cache)
+        .unwrap().clone()
+    )
   } else {
     match util::http_client::make_request::<WOD>(wod_url).await {
       Ok(data) => {
@@ -90,19 +106,27 @@ pub async fn weather_of_day(data: web::Data<Mutex<AppCache>>, info: web::Query<W
           new_cache.clone()
         );
         app_cache.wod_dt = Some(util::datetime::now());
-        HttpResponse::Ok().json(data)
+        Some(data)
       },
-      Err(_err) => HttpResponse::new(StatusCode::from_u16(500).unwrap())
+      Err(_err) => None
     }
+  }
+}
+
+pub async fn weather_of_day(data: web::Data<Mutex<AppCache>>, info: web::Query<WODRequest>) -> impl Responder {
+  match get_wod(data, WODRequest { location: info.location.to_owned() }).await {
+    Some(result) => HttpResponse::Ok().json(result),
+    None => HttpResponse::new(StatusCode::from_u16(500).unwrap())
   }
 }
 /*
   News of day
 */
-pub async fn news_of_day(data: web::Data<Mutex<AppCache>>, info: web::Query<NODRequest>) -> impl Responder {
+
+pub async fn get_nod(data: web::Data<Mutex<AppCache>>, params: NODRequest) -> Option<NOD> {
   let mut app_cache = data.lock().unwrap();
   let base_url: &str = "http://newsapi.org/v2/top-headlines";
-  let resolved_country_code: String = match &info.country {
+  let resolved_country_code: String = match &params.country {
     None => String::from("us"),
     Some(country) => country.to_string(),
   };
@@ -111,8 +135,10 @@ pub async fn news_of_day(data: web::Data<Mutex<AppCache>>, info: web::Query<NODR
   let nod_url: &str = &(base_url.to_owned() + "?country=" + &resolved_country_code.to_owned() + "&apiKey=" + &api_key.to_owned());
   
   if app_cache.nod_exists(resolved_country_code) {
-    HttpResponse::Ok().json(
-      app_cache.nod.as_ref().unwrap().get(&resolved_country_code_cache)
+    Some(
+      app_cache.nod
+        .as_ref().unwrap().get(&resolved_country_code_cache)
+        .unwrap().clone()
     )
   } else {
     match util::http_client::make_request::<NOD>(nod_url).await {
@@ -128,32 +154,62 @@ pub async fn news_of_day(data: web::Data<Mutex<AppCache>>, info: web::Query<NODR
           new_cache.clone()
         );
         app_cache.nod_dt = Some(util::datetime::now());
-        HttpResponse::Ok().json(data)
+        Some(data)
       },
-      Err(_err) => HttpResponse::new(StatusCode::from_u16(500).unwrap())
+      Err(_err) => None
     }
+  }
+}
+
+pub async fn news_of_day(data: web::Data<Mutex<AppCache>>, info: web::Query<NODRequest>) -> impl Responder {
+  match get_nod(data, NODRequest { country: info.country.to_owned() }).await {
+    Some(result) => HttpResponse::Ok().json(result),
+    None => HttpResponse::new(StatusCode::from_u16(500).unwrap())
   }
 }
 
 /*
   History of day
 */
-pub async fn history_of_day(data: web::Data<Mutex<AppCache>>) -> impl Responder {
+
+pub async fn get_hod (data: web::Data<Mutex<AppCache>>) -> Option<HOD> {
   let mut app_cache = data.lock().unwrap();
   let qod_url: &str = "https://history.muffinlabs.com/date";
   if app_cache.hod_exists() {
-    HttpResponse::Ok()
-          .json(app_cache.hod.as_ref())
+    app_cache.hod.clone()
   } else {
     match util::http_client::make_request::<HOD>(qod_url).await {
       Ok(data) => {
         app_cache.hod = Some(data.clone());
         app_cache.hod_dt = Some(util::datetime::now());
-        HttpResponse::Ok()
-          .json(data)
+        Some(data)
       },
-      Err(_err) => HttpResponse::new(StatusCode::from_u16(500).unwrap())
+      Err(_err) => None
     }
   }
 }
+
+pub async fn history_of_day(data: web::Data<Mutex<AppCache>>) -> impl Responder {
+  match get_hod(data).await {
+    Some(result) => HttpResponse::Ok().json(result),
+    None => HttpResponse::new(StatusCode::from_u16(500).unwrap())
+  }
+}
+
+/* Today - Unified API */
+pub async fn today(data: web::Data<Mutex<AppCache>>, info: web::Query<TodayRequest>) -> impl Responder {
+  let qod = get_qod(data.clone()).await;
+  let wod = get_wod(data.clone(), WODRequest { location: info.location.to_owned() }).await;
+  let nod = get_nod(data.clone(), NODRequest { country: info.country.to_owned() }).await;
+  let hod = get_hod(data.clone()).await;
+  HttpResponse::Ok().json::<TodayResponse>(
+    TodayResponse { 
+      qod,
+      wod,
+      nod,
+      hod
+    }
+  )
+}
+
 
--- src/util/environment.rs
+++ src/util/environment.rs
@@ -5,10 +5,7 @@ Fetches the value of any environment variable
 pub fn get_env (key: &str, default_value: Option<&str>) -> String {
   let empty: &str = "";
   match env::var(key) {
-    Ok(val) => {
-      println!("Fetched env data: {}: {}", key, val);
-      String::from(val)
-    },
+    Ok(val) => String::from(val),
     Err(err) => {
       println!("Couldn't interpret env {:?}: {}", key, err);
       match default_value {
