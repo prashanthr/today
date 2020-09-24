91f4d7d3ca5fb8e4a6727d7abc6dafc41fa48cc1
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-09-23 17:51:28 -0700

    cleanup hash fn

--- src/util/hash.rs
+++ src/util/hash.rs
@@ -1,8 +1,5 @@
 use md5;
 
 pub fn compute_hash(input: String) -> String {
-  let cl = input.clone();
-  let hash = String::from(format!("{:x}", md5::compute(input)));
-  println!(">> input {:?} hash {:?}", cl, hash);
-  hash
+  String::from(format!("{:x}", md5::compute(input)))
 } 
