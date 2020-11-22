b2a0d8356161fd78ec0909a0f6f05d1f27d6c20e
Prashanth Rajaram <prashanthr@users.noreply.github.com>
2020-11-21 19:40:44 -0800

    feat(cors): Add cors support (#11)
    Co-authored-by: Prashanth R <1409501+prashanthr@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, http, middleware::Logger};
+use actix_cors::Cors;
+use env_logger::Env;
 use std::sync::Mutex;
 
 mod api;
@@ -9,6 +11,7 @@ use types::AppCache;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let app_name: String = String::from("today");
     let host: String = util::environment::get_env("TODAY_API_HOST", Some("0.0.0.0"));
     let port: String =  util::environment::get_env("TODAY_API_PORT", Some("9000"));
@@ -32,8 +35,18 @@ async fn main() -> std::io::Result<()> {
     println!("Running {} server at {}:{}", app_name, host, port);
 
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allowed_methods(vec!["GET"])
+            .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+            .allowed_header(http::header::CONTENT_TYPE)
+            .supports_credentials()
+            .max_age(3600);
         App::new()
             .app_data(app_data.clone())
+            .wrap(cors)
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .route("/", web::get().to(api::health))
             .route("/health", web::get().to(api::health))
             .route("/debug", web::get().to(api::debug))
