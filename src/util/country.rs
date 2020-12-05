5effa01941a10a814da17ac4a6fdda2bccb857bc
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2020-12-04 17:29:23 -0800

    fix(country): Add default country as fallback

--- src/util/country.rs
+++ src/util/country.rs
@@ -1,8 +1,12 @@
 use celes::Country;
 use std::str::FromStr;
 
+fn get_default_country() -> Country {
+  Country::the_united_states_of_america()
+}
+
 pub fn get_country(country: &str) -> Country {
-  Country::from_str(country).unwrap()
+  Country::from_str(country).unwrap_or(get_default_country())
 }
 
 pub fn get_country_code(country: &str) -> String {
