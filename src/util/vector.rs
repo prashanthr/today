bc63178d3db482b1006731ac1b5b7673ef238bd0
Prashanth R <1409501+prashanthr@users.noreply.github.com>
2021-02-19 19:17:31 -0800

    add docs

--- src/util/vector.rs
+++ src/util/vector.rs
@@ -6,6 +6,12 @@ pub fn get_slice<T: Clone>(data: Vec<T>, start: usize, end: usize) -> Vec<T> {
   data[start..slice_end].to_vec()
 }
 
+/*
+  Get a random element from a list of items
+  Usage:
+  let items = vec![1,2,3];
+  let random_el = get_random(&items);
+*/
 pub fn get_random<T: Clone + std::fmt::Debug>(data: &Vec<T>) -> Option<&T> {
   let chosen_one = data.choose(&mut rand::thread_rng());
   match chosen_one {
